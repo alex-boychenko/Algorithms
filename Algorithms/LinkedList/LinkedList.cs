fc2618631ac702bc6357a8628d13c550f4f460e8
Alex Boychenko <alboychenko@luxoft.com>
2022-07-27 21:33:18 +0300

    add linked list data structure

---
+++ Algorithms/LinkedList/LinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.LinkedList
+{
+    public class LinkedList<T>
+    {
+        public ListNode<T> Head { get; private set; }
+
+        public void AddNode(T data)
+        {
+            var next = new ListNode<T>(data);
+
+            if (Head == null)
+            {
+                Head = next;
+                return;
+            }
+
+            var current = Head;
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            current.Next = next;
+        }
+
+        public void Reverse()
+        {
+            ListNode<T> prev = null;
+            var current = Head;
+
+            // 1 -> 2 -> 3 -> null
+            // null <- 1 <- 2 <- 3
+
+            while (current != null)
+            {
+                var next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+
+            Head = prev;
+        }
+    }
+}
---
+++ Algorithms/LinkedList/ListNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.LinkedList
+{
+    public class ListNode<T>
+    {
+        public T Data { get; set; }
+        public ListNode<T> Next { get; set; }
+
+        public ListNode(T data)
+        {
+            Data = data;
+        }
+    }
+}
